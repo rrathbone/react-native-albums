0130bbc542d451e247c7827a293963705244dd52
Rachelle Rathbone <rachelle@migo.co>
2017-03-11 12:51:56 -0800

    complete build of first react native app

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+
+import AlbumList from './src/components/AlbumList';
+import Header from './src/components/Header';
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Image, Linking, Text, View } from 'react-native';
+
+import Button from './Button';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = ({ album }) => {
+  const { artist, image, thumbnail_image, title, url } = album;
+  const {
+    headerContentStyle,
+    headerTextStyle,
+    imageStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle
+  } = styles;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
+};
+
+export default AlbumDetail;
